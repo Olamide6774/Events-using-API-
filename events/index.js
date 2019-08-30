b00da3f57af189e378363884dc497e25fbacd28d
Olamide6774 <50405389+Olamide6774@users.noreply.github.com>
2019-08-30 11:53:04 +0100

    Add files via upload

---
+++ events/index.js
@@ -0,0 +1,21 @@
+ const express = require('express');
+
+ const app = express();
+
+  const data =[
+    {"id" : 1,
+      "name" :"hajar"
+    },
+    {"id" : 2,
+      "name" :"Temi"
+    },
+    {"id" : 3,
+      "name" :"John"
+    },
+    {"id" : 4,
+      "name" :"Olamide"
+    },
+    {"id" : 5,
+      "name" :" Lanre"
+    }
+];
